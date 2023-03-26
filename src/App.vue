378db257b6f06a1ccbe1dbb4458f824baf5b71da
Noitolar <matianxing2000@163.com>
2023-03-26 16:30:30 +0800

    做了一个简单的表格界面用来测试UI库，效果不太好。

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-    <MyLayout></MyLayout>
+    <MyElementTest></MyElementTest>
 </template>
 
 
 <script setup lang="ts">
-import MyLayout from "./components/MyLayout/index.vue"
+import MyElementTest from "./components/MyElementTest.vue"
 </script>
 
 
 <style lang="less">
-    html, body, #app{
-        height: 100%;
-        overflow: hidden;
-    }
+html, body, #app{
+    height: 100%;
+    overflow: hidden;
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
-import { createApp } from 'vue'
+import {createApp} from "vue"
 // import './style.css'
-import App from './App.vue'
 // import "./assets/css/reset.css"
+import ElementPlus from "element-plus"
+import * as ElementPlusIconsVue from "@element-plus/icons-vue"
+import "element-plus/dist/index.css"
+import App from "./App.vue"
 
-createApp(App).mount('#app')
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue))
+    app.component(key, component)
+app.use(ElementPlus)
+app.mount("#app")
